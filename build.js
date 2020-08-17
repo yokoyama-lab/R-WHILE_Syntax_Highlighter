f12fef0af43ff224f95527c6a08da7ffdb6a7fbb
Yoshi0207 <yoaplid002@hotmail.co.jp>
2020-08-17 21:01:55 +0900

    refactoring

--- build.js
+++ build.js
@@ -13,13 +13,13 @@ const systemCmd = cmdString => {
     cmd.get(
       cmdString,
       (data, err, stderr) => {
-        console.log(cmdString);
-        console.log(data);
+        // console.log(cmdString);
+        // console.log(data);
         if (err) {
-          console.log(err);
+          // console.log(err);
         }
         if (stderr) {
-          console.log(stderr);
+          // console.log(stderr);
         }
         resolve(data);
       }
@@ -30,19 +30,25 @@ const systemCmd = cmdString => {
 co(function* () {
   try {
     // download ace.js 
+    console.log("Download ace");
     yield systemCmd(`git clone https://github.com/ajaxorg/ace.git`);
-
     // cd ./ace and npm install (current dir:R-WHILE_Syntax_Highlighter/ace)
     yield systemCmd(`cd ./ace; npm install`);
+    console.log("Download successful");
+
     // cp R-WHILE_Syntax_Highlighter/rwhile.js R-WHILE_Syntax_Highlighter/ace/lib/ace/mode
-    // rwhile_highlight_rules.js will be copied similarly.
+    // rwhile_highlight_rules.js be copied similarly.
+    console.log("Copy rwhile.js and rwhile_highlight_rules.js to ./ace/lib/ace/mode/");
     fs.copySync(`./rwhile.js`, './ace/lib/ace/mode/rwhile.js');
     fs.copySync(`./rwhile_highlight_rules.js`, './ace/lib/ace/mode/rwhile_highlight_rules.js');
 
     // compile r-while.js
+    console.log("Build mode-rwhile.js");
     yield systemCmd(`node ./ace/Makefile.dryice.js -nc`);
+    console.log("Build successful");
 
     // mkdir ./build
+    console.log("Copy mode-rwhile.js to ./build");
     fs.mkdirsSync(`build`);
     fs.copySync(`./ace/build/src-noconflict/mode-rwhile.js`, './build/mode-rwhile.js');
     // fs.copySync(`./ace/build/src-noconflict/ace.js`, './build/ace.js');
